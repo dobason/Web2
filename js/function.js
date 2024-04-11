d672f459356b61f3b640a186e1143bcf2832fc55
Nhat-Phuong <pnhatphuong2011@gmail.com>
2024-04-11 21:20:09 +0700

    kien cap nhat

--- js/function.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const categoryLink = document.getElementById("categoryLink");
-    const mainContainer = document.querySelector(".main-container");
-
-    categoryLink.addEventListener("click", function () {
-        // Toggle the "active" class on main-container
-        mainContainer.classList.toggle("active");
-    });
-});
