7d608ac294b57b759f893b5e8407df97361621d0
dobason <son2522004@gmail.com>
2024-05-01 07:42:35 +0700

    asd

--- cart.php
+++ cart.php
@@ -436,13 +436,9 @@ foreach ($results as $result) {
 
   
 }
-
-
 ?>
-
-
     
-        <div class="total-row">
+       <div class="total-row">
             <span class="total-label">Tổng cộng:</span>
             <span id="total-price"> </span>
         </div>
