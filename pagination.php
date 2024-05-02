ea640ea84f9a796addbcfb13a02ded5dda6ac5b6
Nhat-Phuong <pnhatphuong2011@gmail.com>
2024-05-02 20:27:42 +0700

    loc chu de va url

--- pagination.php
+++ pagination.php
@@ -1,38 +1,48 @@
 <div id="pagination">
-<?php
-if ($current_page > 3) {
-    $first_page = 1;
-    ?>
-    <a class="page-item" href="?<?=$param?>per_page=<?= $item_per_page ?>&page=<?= $first_page ?>">First</a>
     <?php
-}
-if ($current_page > 1) {
-    $prev_page = $current_page - 1;
+    $queryData = array_merge($param, ['per_page' => $item_per_page]);
+
+    if ($current_page > 3) {
+        $queryData['page'] = 1;
+        ?>
+        <a class="page-item" href="?<?= http_build_query($queryData) ?>">First</a>
+        <?php
+    }
+
+    if ($current_page > 1) {
+        $queryData['page'] = $current_page - 1;
+        ?>
+        <a class="page-item" href="?<?= http_build_query($queryData) ?>">Prev</a>
+        <?php
+    }
+
+    for ($num = 1; $num <= $totalPages; $num++) {
+        if ($num != $current_page) {
+            if ($num > $current_page - 3 && $num < $current_page + 3) {
+                $queryData['page'] = $num;
+                ?>
+                <a class="page-item" href="?<?= http_build_query($queryData) ?>"><?= $num ?></a>
+                <?php
+            }
+        } else {
+            ?>
+            <strong class="current-page page-item"><?= $num ?></strong>
+            <?php
+        }
+    }
+
+    if ($current_page < $totalPages - 1) {
+        $queryData['page'] = $current_page + 1;
+        ?>
+        <a class="page-item" href="?<?= http_build_query($queryData) ?>">Next</a>
+        <?php
+    }
+
+    if ($current_page < $totalPages - 3) {
+        $queryData['page'] = $totalPages;
+        ?>
+        <a class="page-item" href="?<?= http_build_query($queryData) ?>">Last</a>
+        <?php
+    }
     ?>
-    <a class="page-item" href="?<?=$param?>per_page=<?= $item_per_page ?>&page=<?= $prev_page ?>">Prev</a>
-<?php }
-?>
-<?php for ($num = 1; $num <= $totalPages; $num++) { ?>
-    <?php if ($num != $current_page) { ?>
-        <?php if ($num > $current_page - 3 && $num < $current_page + 3) { ?>
-            <a class="page-item" href="?<?=$param?>per_page=<?= $item_per_page ?>&page=<?= $num ?>"><?= $num ?></a>
-        <?php } ?>
-    <?php } else { ?>
-        <strong class="current-page page-item"><?= $num ?></strong>
-    <?php } ?>
-<?php } ?>
-<?php
-if ($current_page < $totalPages - 1) {
-    $next_page = $current_page + 1;
-    ?>
-    <a class="page-item" href="?<?=$param?>per_page=<?= $item_per_page ?>&page=<?= $next_page ?>">Next</a>
-<?php
-}
-if ($current_page < $totalPages - 3) {
-    $end_page = $totalPages;
-    ?>
-    <a class="page-item" href="?<?=$param?>per_page=<?= $item_per_page ?>&page=<?= $end_page ?>">Last</a>
-    <?php
-}
-?>
 </div>
